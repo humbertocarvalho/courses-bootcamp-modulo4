e895b420cd818e48a623f34f464b4197f6f84b8a
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-08-28 21:28:18 -0300

    Add Handle Delete method.

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -21,13 +21,24 @@ export default class TechList extends Component {
       newTech: ''
     });
   };
+
+  handleDelete = tech => {
+    this.setState({
+      techs: this.state.techs.filter(t => t !== tech)
+    });
+  };
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <ul>
-          {this.state.techs.map(tech => {
-            return <li key={tech}> {tech}</li>;
-          })}
+          {this.state.techs.map(tech => (
+            <li key={tech}>
+              {tech}
+              <button onClick={() => this.handleDelete(tech)} type='button'>
+                Remover
+              </button>
+            </li>
+          ))}
         </ul>
         <input
           type='text'
